8fc2c46d9934f411551aa2a9b1655aacbcbb770a
ryok <ryok@example.com>
2020-09-13 01:13:47 +0900

    update

--- main.go
+++ main.go
@@ -16,21 +16,18 @@ func mylog(sts ...interface{}) {
 
 
 func checkDir(dirPath string) {
-	// give dir name in dirPath
-
 	mylog("now is : ", dirPath, " moved to the location! ")
 	os.Chdir(dirPath)
-	listupDir, _ := ioutil.ReadDir(dirPath)
+	listupDir, _ := ioutil.ReadDir(".")
 
 	for _, f := range listupDir {
-		mylog("           listup: f: ", absPath(f.Name()))
+		mylog("  listup: f: ", absPath(f.Name()))
 	}
 
 	for _, filesIn := range listupDir {
 		absP := absPath(filesIn.Name())
 
 		if filesIn.IsDir() == false {
-			//mylog(absP, " is not a directory.")
 			continue
 		}
 
@@ -39,11 +36,11 @@ func checkDir(dirPath string) {
 			continue
 		}
 
-		wg := &sync.WaitGroup{}
+		var wg = &sync.WaitGroup{}
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			checkDir(filesIn.Name())
+			checkDir(absP)
 		}()
 		wg.Wait()
 	}
@@ -51,46 +48,7 @@ func checkDir(dirPath string) {
 
 
 func main() {
-
 	mylog("--- before checkdir")
 	checkDir(".")
 	mylog("--- after  checkdir")
-
-	//dir, _ := os.Getwd()
-	//fpathList := []string{}
-	//
-	//fpathList = append(fpathList, dir)
-	//for {
-	//	if len(fpathList) == 0 {
-	//		break
-	//	}
-	//	lastEl := fpathList[len(fpathList)-1]
-	//	fpathList = fpathList[:len(fpathList)-1]
-	//
-	//
-	//	filesInfos, _ := ioutil.ReadDir(lastEl)
-	//
-	//	for _, f := range filesInfos {
-	//		mylog(f.Name(), f.IsDir(), fmt.Sprint(f.ModTime()), f.Size(), f.Sys())
-	//		st, _ := filepath.Abs(f.Name())
-	//		mylog("absPath: ", st)
-	//
-	//
-	//		if f.IsDir() {
-	//			absPath, _ := filepath.Abs(f.Name())
-	//
-	//			if f.Name() == "build" {
-	//				//os.RemoveAll(absPath)
-	//				println("found build! : ", absPath)
-	//				continue
-	//			}
-	//
-	//			fpathList = append(fpathList, absPath)
-	//		}
-	//	}
-	//
-	//}
-
-
-
 }
